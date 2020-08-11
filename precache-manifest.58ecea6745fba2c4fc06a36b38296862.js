3f28ca210120ffae64423b696a6e7337c2661f67
Josh Homer <joshuachomer@gmail.com>
2020-08-10 21:36:58 -0400

    Updates

---
+++ precache-manifest.58ecea6745fba2c4fc06a36b38296862.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508f64ef8d55e1b27b6e7a11fdcfeb3b",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "7a055c34906f07422082",
+    "url": "/React-Portfolio/static/css/main.2ccd6d99.chunk.css"
+  },
+  {
+    "revision": "08bb82546245d32765b3",
+    "url": "/React-Portfolio/static/js/2.5969fced.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.5969fced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a055c34906f07422082",
+    "url": "/React-Portfolio/static/js/main.266ef95e.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "82578242ef7a11876e530f5a410b67f1",
+    "url": "/React-Portfolio/static/media/RESUME.82578242.pdf"
+  },
+  {
+    "revision": "b0b109f7888a1412a32f506dd9e6f072",
+    "url": "/React-Portfolio/static/media/screenShot1.b0b109f7.jpeg"
+  },
+  {
+    "revision": "89c40585a63eb20412be369f3f17aa5a",
+    "url": "/React-Portfolio/static/media/screenShot2.89c40585.jpeg"
+  },
+  {
+    "revision": "a33be98a2696b6f54639284feb4057b1",
+    "url": "/React-Portfolio/static/media/screenShot3.a33be98a.jpeg"
+  },
+  {
+    "revision": "03e004096d95c58098766a4d08d961de",
+    "url": "/React-Portfolio/static/media/screenShot4.03e00409.jpeg"
+  },
+  {
+    "revision": "ae7ab21228f2e4ae61084ed0287e123b",
+    "url": "/React-Portfolio/static/media/screenShot5.ae7ab212.jpeg"
+  },
+  {
+    "revision": "996b48059d294337980f20c9e12f213f",
+    "url": "/React-Portfolio/static/media/screenShot6.996b4805.jpeg"
+  },
+  {
+    "revision": "36779afcfc3cc500b3bfd484468c3e1c",
+    "url": "/React-Portfolio/static/media/screenShot7.36779afc.jpeg"
+  },
+  {
+    "revision": "9b7af745292f68c48913ebb24de33174",
+    "url": "/React-Portfolio/static/media/screenShot8.9b7af745.jpeg"
+  },
+  {
+    "revision": "1e8d356d6679b02a2f7611e2554ba463",
+    "url": "/React-Portfolio/static/media/screenshot.1e8d356d.jpeg"
+  },
+  {
+    "revision": "78ee9c8a6a9f322010a59e932bc83920",
+    "url": "/React-Portfolio/static/media/wallpaper.78ee9c8a.jpeg"
+  }
+]);
